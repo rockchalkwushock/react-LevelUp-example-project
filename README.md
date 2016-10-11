5dc9c9c5f2ee9d4d0b49b3d6b173ea16d7d858ec
Cody Brunner <rockchalkwushock@icloud.com>
2016-10-11 18:59:47 -0500

    V1.1: Added App & ContactsList Components. Added general data & a few props.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # react-LevelUp-example-project
 Getting a handle on react with LevelUp tutorials
+
+
+## Flow of Components
+
+  1. index.js - renders `<App />`
+  2. App.js
--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ContactsList from './components/ContactsList';
 
+let contacts = {
+  name: 'Cody',
+  phone: '012 345 6789'
+};
 
+class App extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>Contacts List</h1>
+        <ContactsList contacts={this.props.contacts}/>
+      </div>
+    )
+  }
+}
 
 document.addEventListener('DOMContentLoaded', () => {
-    ReactDOM.render(<Board />, document.getElementById('app'));
+    ReactDOM.render(<App contacts={contacts} />, document.getElementById('app'));
 });
